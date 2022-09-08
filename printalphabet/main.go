40f2f91ed1d35091c05faf27ade6704973aab73d
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-08 11:57:22 +0200

    printalphabet1

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,1 +1,15 @@
-package printalphabet
+package main
+
+import (
+	"github.com/01-edu/z01"
+	_ "github.com/01-edu/z01"
+)
+
+func main() {
+	for i := 97; i < 122; {
+
+		z01.PrintRune(rune(i))
+		i++
+	}
+	z01.PrintRune('\n')
+}
