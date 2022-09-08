d6ec903cfa00ae4fbc1c7de77197fb6a8010238d
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-08 15:00:46 +0200

    isnegative1

--- isnegative.go
+++ isnegative.go
@@ -1,25 +1,19 @@
-package main
+package piscine
 
 import (
 	"github.com/01-edu/z01"
 	_ "github.com/01-edu/z01"
 )
 
-func main() {
-	IsNegative(1)
-	IsNegative(0)
-	IsNegative(-1)
-}
-
 func IsNegative(nb int) {
 
 	i := nb
 
-	if i >= 0 {
+	if i < 0 {
 		z01.PrintRune('T')
 		z01.PrintRune('\n')
 
-	} else if i < 0 {
+	} else if i >= 0 {
 		z01.PrintRune('F')
 		z01.PrintRune('\n')
 
