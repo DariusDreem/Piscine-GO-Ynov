3f7a03a7d462588a7027c127edbc807e25335510
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-09 09:19:45 +0200

    printcomb2

---
+++ printcomb2.go
@@ -0,0 +1,35 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	PrintComb2()
+}
+
+func PrintComb2() {
+	for a := 48; a <= 57; a++ {
+		for b := 48; b <= 57; b++ {
+			for c := 48; c <= 57; c++ {
+				for d := 48; d <= 57; d++ {
+					if a == 57 && b == 56 && c == 57 && d == 57 {
+						z01.PrintRune(rune(a))
+						z01.PrintRune(rune(b))
+						z01.PrintRune(' ')
+						z01.PrintRune(rune(c))
+						z01.PrintRune(rune(d))
+						z01.PrintRune('\n')
+						return
+					} else {
+						z01.PrintRune(rune(a))
+						z01.PrintRune(rune(b))
+						z01.PrintRune(' ')
+						z01.PrintRune(rune(c))
+						z01.PrintRune(rune(d))
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
+			}
+		}
+	}
+}
