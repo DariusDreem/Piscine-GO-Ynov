b133dd073bd8863b16ef2b7dd6c88767b2e332df
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-09 10:10:00 +0200

    printcomb2-4

--- printcomb2.go
+++ printcomb2.go
@@ -3,16 +3,16 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for a := '0'; a <= '9'; a++ {
-		for b := '0'; b <= '9'; b++ {
-			for c := '0'; c <= '9'; c++ {
-				for d := '0'; d <= '9'; d++ {
+	for a := 48; a <= 57; a++ {
+		for b := 48; b <= 57; b++ {
+			for c := 48; c <= 57; c++ {
+				for d := 48; d <= 57; d++ {
 					z01.PrintRune(rune(a))
 					z01.PrintRune(rune(b))
 					z01.PrintRune(' ')
 					z01.PrintRune(rune(c))
 					z01.PrintRune(rune(d))
-					if a == '9' && b == '8' && c == '9' && d == '9' {
+					if a == 57 && b == 56 && c == 57 && d == 57 {
 						z01.PrintRune('\n')
 						return
 					} else {
