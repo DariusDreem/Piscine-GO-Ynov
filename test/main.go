859698179364e2ec08ee25fec8e1dd6cf01bad3a
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-12 15:09:01 +0200

    swap

--- test/main.go
+++ test/main.go
@@ -5,12 +5,17 @@ import (
 	"piscine"
 )
 
+package main
+
+import (
+"fmt"
+"piscine"
+)
+
 func main() {
-	a := 13
-	b := 2
-	var div int
-	var mod int
-	piscine.DivMod(a, b, &div, &mod)
-	fmt.Println(div)
-	fmt.Println(mod)
+	a := 0
+	b := 1
+	piscine.Swap(&a, &b)
+	fmt.Println(a)
+	fmt.Println(b)
 }
