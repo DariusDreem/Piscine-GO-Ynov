34ea9be112e5d965d31c3448027dbf4e27bc058d
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-13 11:53:00 +0200

    atoi-4

--- atoi.go
+++ atoi.go
@@ -5,21 +5,19 @@ func Atoi(s string) int {
 	for i := len([]rune(s)) - 1; i >= 0; i-- {
 		rev = rev + string(s[i])
 	}
-	moins := 0
-	plus := 0
 	a := 0
 	i := 1
+	ispositive := false
 	isnegative := false
 	for _, element := range rev {
 		if int(element) < 48 || int(element) > 57 {
-			if int(element) == 43 && plus < 1 {
-				plus++
-			} else if int(element) == 45 && moins == 1 {
+			if int(element) == 43 && ispositive == false {
+				ispositive = true
+			} else if ispositive == true && isnegative == true {
 				return 0
-			} else if int(element) == 43 && plus == 1 {
+			} else if int(element) == 43 && ispositive == true {
 				return 0
-			} else if int(element) == 45 && moins < 1 {
-				moins++
+			} else if int(element) == 45 && isnegative == false {
 				isnegative = true
 			} else {
 				return 0
