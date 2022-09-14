b49b51b394cb5baecf3510c6fd892ec670a0710f
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-14 14:10:24 +0200

    isprime

---
+++ isprime.go
@@ -0,0 +1,14 @@
+package piscine
+
+func IsPrime(nb int) bool {
+	if nb%2 == 0 && nb != 2 {
+		return false
+	} else if nb%3 == 0 && nb != 3 {
+		return false
+	} else if nb%5 == 0 && nb != 5 {
+		return false
+	} else if nb%7 == 0 && nb != 7 {
+		return false
+	}
+	return true
+}
