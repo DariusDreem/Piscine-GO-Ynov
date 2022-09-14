9e52715b98b69dbc0cdf88b6c09b6683a313e06e
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-14 10:13:14 +0200

    iterative power8

--- iterativepower.go
+++ iterativepower.go
@@ -2,11 +2,10 @@ package piscine
 
 func IterativePower(nb int, power int) int {
 	tempo := nb
-	if nb == 0 || power < 0 {
-		return 0
-	}
 	if power == 0 {
 		return 1
+	} else if nb == 0 || power < 0 {
+		return 0
 	}
 	for i := 1; i < power; i++ {
 		nb = tempo * nb
