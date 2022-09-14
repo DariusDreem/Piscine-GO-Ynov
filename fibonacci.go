4bf429fcbaf85512dc402ef16f54a2a18384866b
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-14 11:59:37 +0200

    fibonacci

--- fibonacci.go
+++ fibonacci.go
@@ -1,1 +1,13 @@
 package piscine
+
+func Fibonacci(index int) int {
+	if index < 0 {
+		return -1
+	} else if index == 1 {
+		return 1
+	} else if index == 0 {
+		return 0
+	}
+	nb := Fibonacci(index-1) + Fibonacci(index-2)
+	return nb
+}
