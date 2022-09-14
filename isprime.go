e74a9000eaecc7daeaa2311651408da43cc5eca4
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-14 14:32:09 +0200

    isprime3

--- isprime.go
+++ isprime.go
@@ -1,18 +1,15 @@
 package piscine
 
 func IsPrime(nb int) bool {
-	if nb%2 == 0 && nb != 2 {
+	if nb == 1 {
 		return false
-	} else if nb%3 == 0 && nb != 3 {
-		return false
-	} else if nb%5 == 0 && nb != 5 {
-		return false
-	} else if nb%7 == 0 && nb != 7 {
-		return false
-	} else if nb == 1 {
-		return false
-	} else if nb < 0 {
-		return false
+	}
+	for i := 2; i < nb; i++ {
+		if nb%i == 0 {
+			return false
+		} else {
+			return true
+		}
 	}
 	return true
 }
--- test/main.go
+++ test/main.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.IsPrime(5))
+	fmt.Println(piscine.IsPrime(987149))
 	fmt.Println(piscine.IsPrime(4))
 }
