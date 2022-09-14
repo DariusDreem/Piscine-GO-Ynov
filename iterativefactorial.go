99544a42282b37ddf1d1a209506dca9f0988bebd
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-14 09:07:06 +0200

    iterative3

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,9 +4,10 @@ func IterativeFactorial(nb int) int {
 	result := 1
 	if nb < 0 {
 		return 0
-	}
-	for i := 1; i < nb+1; i++ {
-		result = result * i
+	} else {
+		for i := 1; i < nb+1; i++ {
+			result = result * i
+		}
 	}
 	return result
 }
