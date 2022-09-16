0ce2dd0f42ba7324cf1ba8a66d9f5769ba7997b0
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-16 09:06:15 +0200

    firstrune

--- test/main.go
+++ test/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"github.com/01-edu/z01"
+
 	"piscine"
 )
 
 func main() {
-	fmt.Println(piscine.FindNextPrime(14))
-	fmt.Println(piscine.IsPrime(14))
-	fmt.Println(piscine.FindNextPrime(4))
+	z01.PrintRune(piscine.FirstRune("Hello!"))
+	z01.PrintRune(piscine.FirstRune("Salut!"))
+	z01.PrintRune(piscine.FirstRune("Ola!"))
+	z01.PrintRune('\n')
 }
