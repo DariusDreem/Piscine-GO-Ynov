527add49a1633fc3dfb7ca0c77ab3a18dadce518
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-16 12:17:03 +0200

    islower

---
+++ islower.go
@@ -0,0 +1,13 @@
+package piscine
+
+func IsLower(s string) bool {
+	for _, letter := range s {
+		if letter < 98 || letter > 122 {
+			return false
+		} else {
+			continue
+		}
+	}
+	return true
+}
+}
