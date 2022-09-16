19be40f325a1ab07a51ca930468d03eba1ff85b4
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-16 14:15:30 +0200

    toupper

--- toupper.go
+++ toupper.go
@@ -1,15 +1,13 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
 func ToUpper(s string) string {
 	world := ""
 	for _, letter := range s {
 		if letter >= 97 && letter <= 122 {
 			letter = letter - 32
-			z01.PrintRune(letter)
+			world += string(letter)
 		} else {
-			z01.PrintRune(letter)
+			world += string(letter)
 		}
 	}
 	return world
