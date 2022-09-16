3e7c4ea4d998fd58a87ea23278be66f35f6ee36f
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-16 12:22:43 +0200

    islower1

--- islower.go
+++ islower.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsLower(s string) bool {
 	for _, letter := range s {
-		if letter < 98 || letter > 122 {
+		if letter < 97 || letter > 122 {
 			return false
 		} else {
 			continue
