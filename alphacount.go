c2ad4fac9480c7475f4b0ba988b6633e3f74a550
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-16 10:11:21 +0200

    alphacount1

--- alphacount.go
+++ alphacount.go
@@ -3,11 +3,11 @@ package piscine
 func AlphaCount(s string) int {
 	count := 0
 	for index, letter := range s {
-		if letter > 65 && letter < 90 {
+		if letter >= 65 && letter <= 90 {
 			count++
-		} else if letter > 97 && letter < 122 {
+		} else if letter >= 97 && letter <= 122 {
 			count++
-		} else if index+1 == StrLen(s) {
+		} else if index == StrLen(s) {
 			return count
 		}
 	}
