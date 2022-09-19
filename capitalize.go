387ec93b97c386783ec1c1f1993d53f6c75df640
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-19 10:29:23 +0200

    capitalize

---
+++ capitalize.go
@@ -0,0 +1,32 @@
+package piscine
+
+func Capitalize(s string) string {
+	world := ""
+	pabo := 0
+	for _, letter := range s {
+		if letter >= 97 && letter <= 122 && pabo == 0 {
+			letter = letter - 32
+			pabo++
+			world += string(letter)
+		} else if letter >= 97 && letter <= 122 && pabo != 0 {
+			pabo++
+			world += string(letter)
+		} else if letter >= 65 && letter <= 90 {
+			if pabo != 0 {
+				letter = letter + 32
+				pabo++
+				world += string(letter)
+			} else {
+				pabo++
+				world += string(letter)
+			}
+		} else if letter == 32 || letter == 43 {
+			pabo = 0
+			world += string(letter)
+		} else if letter >= 48 && letter <= 57 {
+			world += string(letter)
+			pabo++
+		}
+	}
+	return world
+}
