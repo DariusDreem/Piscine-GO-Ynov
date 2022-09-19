62695fc78eaa9aec0e5babe2962acbf970a538ed
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-19 14:25:56 +0200

    printnbrbase

---
+++ printnbrbase.go
@@ -0,0 +1,52 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func PrintNbrBase(nbr int, base string) {
+	isnegativeNbr := 0
+	baseList := []string{}
+	loop := 0
+	resultBase := ""
+	if nbr < 0 {
+		isnegativeNbr = 1
+		nbr = -nbr
+	}
+	if len(base) < 2 {
+		z01.PrintRune('N')
+		z01.PrintRune('V')
+		return
+	}
+	for _, element := range base {
+		loop = 0
+		if len(baseList) == 0 {
+			baseList = append(baseList, string(element))
+			continue
+		}
+		for _, baseLetter := range baseList {
+			if string(element) == baseLetter {
+				z01.PrintRune('N')
+				z01.PrintRune('V')
+				return
+			} else {
+				continue
+			}
+		}
+		if loop == 0 {
+			baseList = append(baseList, string(element))
+		}
+	}
+	for nbr = nbr; nbr != 0; {
+		resultmod := nbr % len(base)
+		resultBase = string(base[resultmod]) + resultBase
+		nbr /= len(base)
+	}
+	for i := 0; i < len(resultBase); i++ {
+		if isnegativeNbr == 1 {
+			z01.PrintRune('-')
+			isnegativeNbr = 0
+		}
+		z01.PrintRune(rune(resultBase[i]))
+	}
+}
