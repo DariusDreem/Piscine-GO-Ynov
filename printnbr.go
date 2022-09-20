6d355222ec4946862651d9dc0c1f5fdcc44b4866
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-20 08:49:34 +0200

    printnbr

--- printnbr.go
+++ printnbr.go
@@ -2,19 +2,28 @@ package piscine
 
 import (
 	"github.com/01-edu/z01"
-	_ "github.com/01-edu/z01"
 )
 
-func PrintNbr(nb int) {
-	newSlice := []int{}
-
-	if nb > 0 || nb < 0 {
-		a := nb % 10
-		newSlice = append(newSlice, a+48)
-		z01.PrintRune(rune(a + 48))
-		nb /= 10
-		PrintNbr(nb)
-	} else {
+func PrintNbr(n int) {
+	resultMod := 0
+	oldResultList := []int{}
+	isNegative := false
+	if n == 0 {
 		z01.PrintRune('0')
+	}
+	if n < 0 {
+		isNegative = true
+		z01.PrintRune('-')
+	}
+	for i := 0; n != 0; i++ {
+		resultMod = n % 10
+		if isNegative == true {
+			resultMod *= -1
+		}
+		oldResultList = append(oldResultList, resultMod)
+		n /= 10
+	}
+	for i := len(oldResultList) - 1; i != -1; i-- {
+		z01.PrintRune(rune(oldResultList[i] + 48))
 	}
 }
