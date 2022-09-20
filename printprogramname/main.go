c030868ef631ecfb33003210f9d50cf40c9e242e
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-20 11:25:06 +0200

    printprogramme

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -15,10 +15,10 @@ func remove_string(str string) {
 	resultList := []int{}
 	for i := len(str) - 1; i != -1; i-- {
 		el := int(str[i])
-		if 65 <= el && 90 >= el || 97 <= el && 122 >= el || el == 46 {
-			resultList = append(resultList, el)
-		} else {
+		if el == 92 || el == 47 || el == 58 || el == 42 || el == 63 || el == 34 || el == 60 || el == 62 || el == 124 {
 			break
+		} else {
+			resultList = append(resultList, el)
 		}
 	}
 	for i := len(resultList) - 1; i != -1; i-- {
