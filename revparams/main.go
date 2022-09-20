604faa8aee73578d380808e5d866c1680c29e6d3
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-20 15:16:05 +0200

    revparam

---
+++ revparams/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arg := os.Args
+	for i := len(arg) - 1; i != 0; i-- {
+		remove_string(arg[i])
+	}
+}
+func remove_string(str string) {
+	for _, el := range str {
+		z01.PrintRune(el)
+	}
+	z01.PrintRune('\n')
+	return
+}
