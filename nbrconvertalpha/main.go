feece66389b7f598ead955b247278e958708cda0
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-22 09:50:12 +0200

    convertalpha

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/asn1"
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+	for _,letter := range os.Args >{
+	if os.Args[1] == "--upper" {
+
+	} else if
+		if n+96 < 97 || n+96 > 122 {
+			z01.PrintRune(32)
+		} else {
+			n += 96
+			z01.PrintRune(rune(n))
+		}
+	}
+}
