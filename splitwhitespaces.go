3df21b163d0122d219b3c276d8dd416f2c10dcb9
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-23 13:54:17 +0200

    aled0

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -4,14 +4,12 @@ func SplitWhiteSpaces(s string) []string {
 	str := ""
 	i := 0
 	array := []string{}
-
 	yee := false
 	for _, word := range s {
 		word = word
 		i++
 		if word == 32 {
 			if str == "" {
-
 			} else {
 				array = append(array, str)
 				str = ""
