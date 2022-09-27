e6b2121bd41d7b2ff8f1273db889919f9c330272
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-27 11:19:08 +0200

    boolean

---
+++ boolean/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func even(nbr2 int) bool {
+	if nbr2%2 == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func isEven(nbr int) bool {
+	if even(nbr) == true {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	arg := os.Args[1:]
+	length := len(arg)
+	EvenMsg := "I have an even number of arguments"
+	OddMsg := "I have an odd number of arguments"
+	if isEven(length) == true {
+		printStr(EvenMsg)
+	} else {
+		printStr(OddMsg)
+	}
+}
