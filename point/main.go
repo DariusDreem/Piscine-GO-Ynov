a08e0470ecfd5be278aa8d1e08ba7a257e2063a5
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-27 12:41:54 +0200

    point

---
+++ point/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"piscine"
+)
+
+type point struct {
+	y int
+	x int
+}
+
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
+}
+
+func main() {
+	points := point{}
+
+	setPoint(&points)
+
+	piscine.PrintStr("x =")
+	z01.PrintRune(rune(points.x/10 + 48))
+	z01.PrintRune(rune(points.x%10 + 48))
+	piscine.PrintStr(", ")
+	piscine.PrintStr("y =")
+	z01.PrintRune(rune(points.y/10 + 48))
+	z01.PrintRune(rune(points.y%10 + 48))
+}
