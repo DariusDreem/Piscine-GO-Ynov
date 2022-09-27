fa9862c91d5fd1f7f7aeb6e6fa7c9157649fe7a7
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-27 13:51:00 +0200

    point1

--- point/main.go
+++ point/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"piscine"
-
 	"github.com/01-edu/z01"
 )
 
 type point struct {
 	y int
 	x int
+}
+
+func PrintStr(s string) {
+	for _, a := range s {
+		z01.PrintRune(a)
+	}
 }
 
 func setPoint(ptr *point) {
@@ -19,11 +23,11 @@ func setPoint(ptr *point) {
 func main() {
 	points := &point{}
 	setPoint(points)
-	piscine.PrintStr("x = ")
+	PrintStr("x = ")
 	z01.PrintRune(rune(points.x/10 + 48))
 	z01.PrintRune(rune(points.x%10 + 48))
-	piscine.PrintStr(", ")
-	piscine.PrintStr("y = ")
+	PrintStr(", ")
+	PrintStr("y = ")
 	z01.PrintRune(rune(points.y/10 + 48))
 	z01.PrintRune(rune(points.y%10 + 48))
 }
