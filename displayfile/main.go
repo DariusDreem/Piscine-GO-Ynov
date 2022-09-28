a1762149471832642dcd467baf3455d8beed9a6d
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-28 17:22:51 +0200

    displayfile

---
+++ displayfile/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	content, err := ioutil.ReadFile(os.Args[1])
+	if len(os.Args[1:]) >= 2 {
+		fmt.Println("Too many arguments")
+	} else if len(os.Args[1:]) == 0 {
+		fmt.Println("File name missing")
+	}
+
+	fmt.Printf("File contents: %s", content)
+
+}
