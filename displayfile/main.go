a412ba8a93b645b460d9b0fdba32318edd632cb9
Nathan Reungoat <reungoat.nathan@gmail.com>
2022-09-29 14:49:11 +0200

    displayfile1

--- displayfile/main.go
+++ displayfile/main.go
@@ -1,18 +1,25 @@
-func main() {
-arg := os.Args
-file, err := os.ReadFile("quest8.txt")
+package main
 
-if len(arg) < 2 {
-fmt.Println("File name missing")
-return
-}
-if len(arg) > 2 {
-fmt.Println("Too many arguments")
-return
-}
-if err != nil {
-fmt.Println(err.Error())
-return
-}
-fmt.Print(string(file))
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	arg := os.Args
+	file, err := os.ReadFile("quest8.txt")
+
+	if len(arg) < 2 {
+		fmt.Println("File name missing")
+		return
+	}
+	if len(arg) > 2 {
+		fmt.Println("Too many arguments")
+		return
+	}
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Print(string(file))
 }
